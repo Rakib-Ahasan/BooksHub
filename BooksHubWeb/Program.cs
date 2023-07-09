34c234568cfda12c6b33a05153cce45f5d277b2b
Rakib Ahasan <bd.rakibahasan@gmail.com>
2023-07-09 21:51:11 +0600

    update

--- BooksHubWeb/Program.cs
+++ BooksHubWeb/Program.cs
@@ -1,8 +1,13 @@
+using BooksHubWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
